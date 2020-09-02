f1ae5fe76360193dc9370a1755d361e6f3d9a66f
saurabhvishk <isaurabhv@gmail.com>
2020-09-03 01:07:54 +0530

    Scene 1 big bang

---
+++ Assets/Script/Expanding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Expanding  : MonoBehaviour 
+{   
+    public float limitScaleX = 200f;
+    SpriteRenderer spriteRenderer;
+    Color colorVal;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Random.ColorHSV(0f,1f,1f,1f,0.5f,1f);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        this.transform.localScale += new Vector3 (0.5f,0.5f,0.5f);
+        if(this.transform.localScale.x >= limitScaleX)
+        {   
+            this.transform.localScale += new Vector3 (1f,1f,1f);
+            
+            
+        }
+
+        if(this.transform.localScale.x >= 100000f)
+        {
+            Destroy(this.gameObject);
+        }
+
+       }
+}
---
+++ Assets/Script/spawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    
+    public Transform spawnPos ;
+    public GameObject spawnObj;
+    float timer = 0f;
+    void Start()
+    {
+        UniversePop();
+    }
+
+    void FixedUpdate()
+    {
+        timer =(timer +Time.deltaTime*50) ;
+        Debug.Log(Time.deltaTime);
+        if(timer % 100 == 0)
+        {   //Debug.Log(timer);
+            UniversePop();
+            
+        }
+    }
+    
+    public void UniversePop()
+    {
+        Instantiate(spawnObj,spawnPos.position, spawnPos.rotation);
+    }
+
+    
+    }
